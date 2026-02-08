641b6b4b2bfb71cb16bf6f9cfcab708f0ef87ce5
André Koschnik <andrekosch98@gmail.com>
2026-02-08 14:19:48 -0300

    ignoring mongo_connection

--- src/db/mongo_connection.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-import os
-
-def get_db():
-    uri = os.getenv("MONGO_URI", "CONNECTION STRING HERE")
-    client = MongoClient(uri)
-    return client["DATABASE NAME HERE"]
-
